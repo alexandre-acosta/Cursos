c1b0fe5229e884f7c81902120f36aec569f643a4
Alexandre Costa <58087731+alexandre-acosta@users.noreply.github.com>
2025-10-20 17:31:07 -0300

    Upload

--- BootCamp - DevOps/logica-js-projeto/app.js
+++ BootCamp - DevOps/logica-js-projeto/app.js
@@ -1,1 +1,9 @@
-alert('Seja Bem Vindo')
+alert('Boas vindas ao jogo do número secreto');
+let numeroSecreto = 5;
+let chute = prompt('Escolha um número entre 1 e 10');
+
+if (chute == numeroSecreto) {
+    alert(`Isso ai! Você descobriu o número secreto: ${numeroSecreto}` );
+} else {
+    alert('Tente novamente')
+}
